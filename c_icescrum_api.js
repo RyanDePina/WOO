4c1ab631cf6fec186624b19e9004d3449835b8dd
XCarrel <Xavier.Carrel@cpnv.ch>
2023-10-16 09:24:08 +0200

    chore(game)Change IceScrum master project code

--- c_icescrum_api.js
+++
@@ -1,9 +0,0 @@
-// A token is required to authenticate your requests
-// Get it from IceScrum > MyAccount > Account > API tokens
-var iceScrumToken = "e40edb162e7d479da7048b4066c9575b";
-
-// Get your project ID from project > Settings > Your Project > Key
-projectId = "SPICY12";
-
-// Select the backend to use
-const API_base_url = "https://etml.icescrum.com/ws/project/";
